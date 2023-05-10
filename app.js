1b788a9c2eae206cee7799febf7ce285a6594373
Gabriel Mascaretti <gabrielmascaretti@gmail.com>
2023-05-10 18:40:40 -0300

    Cambios en pagina de registro

--- app.js
+++ app.js
@@ -11,5 +11,21 @@ app.listen(3000, () => {
 });
 
 app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'))
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+});
+
+app.get('/productCart', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/productCart.html'))
+});
+
+app.get('/productDetail', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
+});
+
+app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'))
 });
